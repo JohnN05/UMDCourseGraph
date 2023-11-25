18fa58ae865ee060833a0fa51198a1f2cbab5a7a
JohnN05 <johnng0511@gmail.com>
2023-11-25 01:38:44 -0500

    Created CourseFactory class.  Finished writing processPrereqs().

--- src/main/java/courses/Course.java
+++ src/main/java/courses/Course.java
@@ -1,11 +1,9 @@
 package courses;
 
-import com.google.gson.Gson;
-import courses.relationship.CourseRelationship;
-import departments.Department;
-import departments.DepartmentList;
+import courses.relationship.requisites.Requisite;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * courses.Course class is used to represent every UMD course.
@@ -13,192 +11,24 @@ import java.util.*;
  *
  * @author JohnN05
  */
-public class Course implements Comparable<Course>, CourseRelationship {
-
-    //Keywords used to interpret course requisites
-    private static final String[] CONJUNCTIONS = {"OR", "AND"};
-    private static final HashMap<String, String> KEYWORDS = new HashMap<>();
-    static{
-        KEYWORDS.put("EARNED", "EXAM"); //for prerequisite exams
-        KEYWORDS.put("HIGH","COURSE");  //for high school prerequisite
-    }
-
-    private static final String[] RELATIONSHIPS = {"coreqs", "prereqs", "restrictions", "additional_info"};
-    private static final String DEFAULT_SEARCH_QUERIES = "&_openSectionsOnly=on&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL";
-    private static final int COURSE_ID_LENGTH = 7;  //length of courses.Course IDs
-    private static final Gson gson = new Gson();
-
-    private String course_id;
-    private String name;
-    private int credits;
-    private String description;
-    private Set<Set<String>> gen_eds;
-    private Set<Set<CourseRelationship>> prereqs;
-    private Set<Set<CourseRelationship>> coreqs;
+public class Course implements Comparable<Course> {
+    private final String course_id;
+    private final String name;
+    private final int credits;
+    private final String description;
+    private final Set<Set<String>> gen_eds;
+    private final HashSet<HashSet<Requisite>> prereqs;
+    private final HashSet<HashSet<Requisite>> coreqs;
 
 
-
-    /**
-     * Constructor for courses.Course.  Primarily used to locate a courses.Course to process.
-     *
-     * @param course_id course_id of this object
-     */
-    public Course(String course_id){
+    public Course(String course_id, String name, int credits, String description, Set<Set<String>> gen_eds, HashSet<HashSet<Requisite>> prereqs, HashSet<HashSet<Requisite>> coreqs) {
         this.course_id = course_id;
-    }
-
-    /**
-     * Processes a courses.RawCourse to obtain the remaining missing information
-     *
-     * @param raw courses.RawCourse that will be converted into a courses.Course
-     */
-    public void processCourse(RawCourse raw){
-        credits = raw.getCredits();
-        description = raw.getDescription();
-
-        processGenEds(raw);
-
-    }
-
-    /**
-     * Processes and interprets the List of Gen-Eds in a courses.RawCourse.  Gen-Ed Sets larger than 1 means the
-     * course provides credits that a student can choose from.
-     * courses.Course will store Gen-Ed credits as such.  Every set in gen_eds represents a single Gen-Ed credit
-     * that can be earned.  If there is more than a single String in a Set, the student may decide which
-     * Gen-Ed they would like to earn for the course.
-     *
-     * @param raw the courses.RawCourse containing the Gen Eds that will be processed
-     */
-    private void processGenEds(RawCourse raw){
-        ArrayList<ArrayList<String>> rawGenEds = raw.getGen_ed();
-        gen_eds = new HashSet<>();
-        HashSet<String> genEdSet;
-
-        //Handles courses that can provide different gen eds
-        if(rawGenEds.size() > 1){
-            genEdSet = new HashSet<>();
-            genEdSet.add(rawGenEds.remove(0).get(0));
-            genEdSet.add(rawGenEds.get(0).remove(0));
-
-            gen_eds.add(genEdSet);
-        }
-
-        if(!rawGenEds.isEmpty()){
-            for(String genEd: rawGenEds.get(0)){
-                genEdSet = new HashSet<>();
-                genEdSet.add(genEd);
-
-                gen_eds.add(genEdSet);
-            }
-        }
-    }
-
-    /**
-     * Processes and interprets the prerequisites stored in the relationships map.  Formats the prerequisites
-     * into a set of courses.relationship.CourseRelationship sets.  Every set in prereqs represents an individual requirement the
-     * student must have prior to taking the class.  If there is more than a single courses.relationship.CourseRelationship in a set,
-     * Only one of the CourseRelationships listed are needed.
-     *
-     * @param raw the courses.RawCourse containing the prerequisites that will be processed
-     */
-    private void processPrereqs(RawCourse raw){
-        prereqs = new HashSet<>();
-        Map<String, String> curRelations = raw.getRelationships();
-        String rawPrereqs = curRelations.get(RELATIONSHIPS[1]);
-
-        if(rawPrereqs == null){
-            return;
-        }
-
-        //Due to the formatting of umd.io's prereqs, prereqs listed with more than one sentence are put in additional_info
-        String morePrereqs = curRelations.get(RELATIONSHIPS[3]);
-        if(morePrereqs != null){
-            String firstWord = morePrereqs.substring(0, morePrereqs.indexOf(" ")).toUpperCase();
-
-            //Identifies displaced prerequisite info with the use of specific conjunctions
-            for(String s: CONJUNCTIONS){
-                if(s.matches(firstWord)){
-                    rawPrereqs += " " + morePrereqs.substring(0, morePrereqs.indexOf("."));
-                }
-            }
-        }
-
-        rawPrereqs = rawPrereqs.replaceAll("-", " ");
-        rawPrereqs = rawPrereqs.replaceAll("\\p{Punct}", "").toUpperCase();
-        LinkedList<String> rawPrereqWords = new LinkedList<>(Arrays.asList(rawPrereqs.split(" ")));
-        LinkedList<String> prereqWords= new LinkedList<>();
-        String curWord;
-
-        //recombines certain prerequisite words
-        while(!rawPrereqWords.isEmpty()){
-            boolean combinedWords = false;
-            curWord = rawPrereqWords.getFirst();
-            String merged = "";
-
-
-            //recombines words that mention department permission.  Example: "Permission of ENGR"
-            if(rawPrereqWords.getFirst().contains("PERMISSION")){
-
-                for(int word = 0; word < 2; word++){
-                    merged += rawPrereqWords.removeFirst() + " ";
-                }
-                prereqWords.add(merged.trim());
-            }
-
-            //recombines words in the list between a keyword and its value
-            if(!combinedWords) {
-                for (String key : KEYWORDS.keySet()) {
-                    int endPos = rawPrereqWords.indexOf(KEYWORDS.get(key));
-
-                    if (rawPrereqWords.contains(key) && endPos != -1) {
-                        combinedWords = true;
-
-                        for (int word = 0; word < endPos; word++) {
-                            merged += rawPrereqWords.removeFirst() + " ";
-                        }
-
-                        prereqWords.add(merged.trim());
-                        break;
-                    }
-                }
-            }
-        }
-
-        //Applies a whitelist on prereqWords
-        Iterator<String> iter = prereqWords.iterator();
-        while(iter.hasNext()){
-            boolean keepWord = false;
-            curWord = iter.next();
-
-            //Keeps department permission requisites
-            if(curWord.contains("PERMISSION")){
-                keepWord = true;
-
-            }if(!keepWord){
-
-                //Keeps conjunction words
-                for(String conjunction: CONJUNCTIONS){
-                    if(curWord.matches(conjunction)){
-                        keepWord = true;
-                        break;
-                    }
-                }
-
-            }if(!keepWord){
-
-                //Keeps words containing department ids such as course ids
-                for(Department d: DepartmentList.getAllDepartments()){
-                    String departmentID = d.getDept_id();
-                    if(curWord.length() == COURSE_ID_LENGTH && curWord.substring(0,4).equals(departmentID)){
-                        keepWord = true;
-                        break;
-                    }
-                }
-
-            }if(!keepWord){
-                iter.remove();
-            }
-        }
+        this.name = name;
+        this.credits = credits;
+        this.description = description;
+        this.gen_eds = gen_eds;
+        this.prereqs = prereqs;
+        this.coreqs = coreqs;
     }
 
     public String getCourse_id() {
@@ -221,11 +51,11 @@ public class Course implements Comparable<Course>, CourseRelationship {
         return gen_eds;
     }
 
-    public Set<Set<CourseRelationship>> getPrereqs() {
+    public HashSet<HashSet<Requisite>> getPrereqs() {
         return prereqs;
     }
 
-    public Set<Set<CourseRelationship>> getCoreqs() {
+    public HashSet<HashSet<Requisite>> getCoreqs() {
         return coreqs;
     }
 
---
+++ src/main/java/courses/CourseFactory.java
@@ -0,0 +1,277 @@
+package courses;
+
+import courses.relationship.requisites.Requisite;
+import courses.relationship.requisites.RequisiteFactory;
+
+import java.util.*;
+
+public class CourseFactory {
+    //Keywords used to interpret course requisites
+    private static final String[] CONJUNCTIONS = {"OR", "AND"};
+    private static final HashMap<String, String> KEYWORDS = new HashMap<>();
+    static{
+        KEYWORDS.put("EARNED", "EXAM"); //for prerequisite exams
+        KEYWORDS.put("HIGH","COURSE");  //for high school prerequisite
+    }
+
+    private static final String[] RELATIONSHIP_KEYS = {"coreqs", "prereqs", "restrictions", "additional_info"};
+
+
+    public static Course processCourse(RawCourse course){
+        String name = course.getName();
+        String course_id = course.getCourse_id();
+        int credits = course.getCredits();
+        String description = course.getDescription();
+        Set<Set<String>> gen_eds = processGenEds(course);
+        HashSet<HashSet<Requisite>> prereqs = processPrereqs(course);
+        return new Course(name, course_id, credits, description, gen_eds, prereqs, null);
+
+    }
+
+    /**
+     * Processes and interprets the List of Gen-Eds in a RawCourse
+     *
+     * @param course the courses.RawCourse containing the Gen Eds that will be processed
+     * @return the formatted set of String set Gen Eds.
+     * Every set in gen_eds represents a single Gen-Ed credit that can be earned.
+     * If there is more than a single String in a Set, the student may decide which Gen-Ed they would like
+     * to earn for the course.
+     */
+    private static Set<Set<String>> processGenEds(RawCourse course){
+        Set<Set<String>> gen_eds = new HashSet<>();
+        ArrayList<ArrayList<String>> rawGenEds = course.getGen_ed();
+        HashSet<String> singleCredit;
+
+        //Handles courses that can provide different gen eds as one credit
+        if(rawGenEds.size() > 1){
+            singleCredit = new HashSet<>();
+            singleCredit.add(rawGenEds.remove(0).get(0));
+            singleCredit.add(rawGenEds.get(0).remove(0));
+
+            gen_eds.add(singleCredit);
+        }
+
+        if(!rawGenEds.isEmpty()){
+            for(String genEd: rawGenEds.get(0)){
+                singleCredit = new HashSet<>();
+                singleCredit.add(genEd);
+
+                gen_eds.add(singleCredit);
+            }
+        }
+
+        return gen_eds;
+    }
+
+    /**
+     * Processes and interprets the prerequisites stored in the relationships map.  Formats the prerequisites
+     * into a set of Requisite sets.
+     *
+     * @param course the RawCourse containing the prerequisites that will be processed
+     * @return the formatted set of Requisites.
+     * Every set in the set represents a single prerequisite that must be fulfilled prior to taking the course.
+     * Requisite sets with size greater than 1 means that the prerequisite can be fulfilled through different ways.
+     */
+    private static HashSet<HashSet<Requisite>> processPrereqs(RawCourse course) {
+        HashSet<HashSet<Requisite>> prereqs = new HashSet<>();
+        Map<String, String> curRelations = course.getRelationships();
+
+        if (!curRelations.containsKey(RELATIONSHIP_KEYS[1]) || curRelations.get(RELATIONSHIP_KEYS[1]) == null) {
+            return prereqs;
+        }
+
+        List<String> prereqWords = getPrereqWords(curRelations);
+        filterReqWords(prereqWords);
+        prereqs = interpretReqWords(prereqWords);
+        return prereqs;
+    }
+
+    /**
+     * Converts the prerequisite sentence provided by the API into separate words.
+     *
+     * @param relationships the Map of relationship Strings containing the prerequisites
+     * @return a List of Strings which represents the words in the prerequisite description.
+     * Certain words are recombined if they contain KEYWORDS for future filtering
+     */
+    private static List<String> getPrereqWords(Map<String, String> relationships){
+        StringBuilder rawPrereqs = new StringBuilder(relationships.get(RELATIONSHIP_KEYS[1]));
+        String morePrereqs = relationships.get(RELATIONSHIP_KEYS[3]);
+
+        //Prerequisite descriptions longer than one sentence are moved into additional information
+        if (morePrereqs != null) {
+            String firstWord = morePrereqs.substring(0, morePrereqs.indexOf(" ")).toUpperCase();
+
+            //Identifies displaced prerequisite info with the use of specific conjunctions
+            for (String s : CONJUNCTIONS) {
+                if (s.matches(firstWord)) {
+                    rawPrereqs.append(" ").append(morePrereqs.substring(0, morePrereqs.indexOf(".")));
+                }
+            }
+        }
+
+        //separates department abbreviations from names for simple processing and removes punctuation
+        rawPrereqs = new StringBuilder(rawPrereqs.toString().replaceAll("-", " "));
+        rawPrereqs = new StringBuilder(rawPrereqs.toString().replaceAll("\\p{Punct}", "").toUpperCase());
+        LinkedList<String> rawPrereqWords = new LinkedList<>(Arrays.asList(rawPrereqs.toString().split(" ")));
+        LinkedList<String> prereqWords = new LinkedList<>();
+
+        //recombines certain prerequisite words
+        while (!rawPrereqWords.isEmpty()) {
+            boolean combinedWords = false;
+            String curWord = rawPrereqWords.getFirst();
+            StringBuilder merged = new StringBuilder();
+
+
+            //recombines words that mention department permission.  Example: "Permission of ENGR"
+            if (curWord.contains("PERMISSION")) {
+                combinedWords = true;
+
+                for (int word = 0; word < 2; word++) {
+                    merged.append(rawPrereqWords.removeFirst()).append(" ");
+                }
+                prereqWords.add(merged.toString().trim());
+            }
+
+            //recombines words in the list between a keyword and its value
+            if (!combinedWords) {
+                for (String key : KEYWORDS.keySet()) {
+                    int endPos = rawPrereqWords.indexOf(KEYWORDS.get(key));
+
+                    if (curWord.contains(key) && endPos != -1) {
+
+                        for (int word = 0; word < endPos; word++) {
+                            merged.append(rawPrereqWords.removeFirst()).append(" ");
+                        }
+
+                        prereqWords.add(merged.toString().trim());
+                        break;
+                    }
+                }
+            }
+        }
+        return prereqWords;
+    }
+
+    /**
+     * Filters words to make requisite interpretation easier
+     * 
+     * @param reqWords the list of words which the filter is applied
+     */
+    private static void filterReqWords(List<String> reqWords){
+        Iterator<String> iter = reqWords.iterator();
+
+        while(iter.hasNext()){
+            boolean keepWord = false;
+            String curWord = iter.next();
+
+            //Keeps department permission requisites
+            if(curWord.contains("PERMISSION")){
+                keepWord = true;
+
+            //Keeps conjunctions
+            }if(!keepWord){
+                
+                for(String conjunction: CONJUNCTIONS){
+                    if(curWord.matches(conjunction)){
+                        keepWord = true;
+                        break;
+                    }
+                }
+                
+            //Keeps courses
+            }if(CourseList.isCourseID(curWord)){
+                keepWord = true;
+
+
+            }if(!keepWord){
+                iter.remove();
+            }
+        }
+    }
+
+    /**
+     * Interprets a list of requisite words into a Set of Requisite Sets
+     *
+     * @param reqWords the list of words being interpretted
+     * @return a formatted Set of Requisite Sets.
+     * Course names are converted into CourseReqs while Department
+     * permission requisites are converted into DepartmentReqs.  Any other Requisites are converted into OtherReqs.
+     * <p>
+     * CONJUNCTIONS are used to identify which Requisites are placed in each Requisite Set
+     * <p>
+     * If there is an "OR" CONJUNCTION, all Requisites before it are added into the Set.  As long as the CONJUNCTION
+     * afterwards continues to be the "OR" CONJUNCTION, all Requisites before it will continue to be added.
+     * When the condition is not met, the Requisite set will be concluded with the last Requisite after the
+     * last "OR" conjunction.
+     * <p>
+     * If there is an "AND" CONJUNCTION, all Requisites before the CONJUNCTION will have their individual Sets
+     * before being added to the main Set.
+     */
+    private static HashSet<HashSet<Requisite>> interpretReqWords(List<String> reqWords){
+        HashSet<HashSet<Requisite>> requisites = new HashSet<>();
+        int conjunctionIndex = indexOfConjunction(reqWords);
+
+        while(!reqWords.isEmpty()){
+            HashSet<Requisite> singleReqCredit = new HashSet<>();
+
+            if(conjunctionIndex == 0){
+                reqWords.remove(0);
+
+            }else if(conjunctionIndex != -1){
+
+                //current CONJUNCTION is "OR"
+                if(reqWords.get(conjunctionIndex).equals(CONJUNCTIONS[0])){
+
+                    //continues adding requisites to the singleReqCredit if the only conjunction is "OR"
+                    do{
+                        //adds all the requisites before "OR"
+                        for(int wordCount = 0; wordCount < conjunctionIndex; wordCount++){
+                            singleReqCredit.add(RequisiteFactory.getRequisite(reqWords.remove(0)));
+                        }
+                        reqWords.remove(0);
+                        conjunctionIndex = indexOfConjunction(reqWords);
+                    }while(conjunctionIndex > 0 && reqWords.get(conjunctionIndex).equals(CONJUNCTIONS[0]));
+
+                    //adds the requisite after "OR" if it exists
+                    if(indexOfConjunction(reqWords) != 0 && !reqWords.isEmpty()){
+                        singleReqCredit.add(RequisiteFactory.getRequisite(reqWords.remove(0)));
+                    }
+                    requisites.add(singleReqCredit);
+
+                //current CONJUNCTION is "AND"
+                }else if(reqWords.get(conjunctionIndex).equals(CONJUNCTIONS[1])){
+
+                    //adds all the requisites before the "AND" as individual sets
+                    for(int wordCount = 0; wordCount < conjunctionIndex; wordCount++){
+                        singleReqCredit = new HashSet<>();
+                        singleReqCredit.add(RequisiteFactory.getRequisite(reqWords.remove(0)));
+                        requisites.add(singleReqCredit);
+                    }
+                    reqWords.remove(0);
+
+                }
+
+            //No CONJUNCTIONS remaining
+            }else{
+                singleReqCredit.add(RequisiteFactory.getRequisite(reqWords.remove(0)));
+                requisites.add(singleReqCredit);
+            }
+        }
+        return requisites;
+    }
+
+    /**
+     * Provides the index of the first instance of a CONJUNCTION in a list
+     * @param words the list of words that will be traverse
+     * @return the index of the first CONJUNCTION.  If CONJUNCTION doesn't exist, returns -1.
+     */
+    private static int indexOfConjunction(List<String> words){
+        int currentOp = words.indexOf(CONJUNCTIONS[0]);
+        int altOp = words.indexOf(CONJUNCTIONS[1]);
+        if(currentOp == -1 || (altOp != -1 && altOp < currentOp)){
+            currentOp = altOp;
+        }
+        return currentOp;
+    }
+
+}
