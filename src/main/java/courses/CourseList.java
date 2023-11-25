105c9c60135b7193a5e9f4a07a7323be21043f0c
JohnN05 <johnng0511@gmail.com>
2023-11-25 00:01:58 -0500

    Added isCourseID() which checks if a String fits as a Course's ID

--- src/main/java/courses/CourseList.java
+++ src/main/java/courses/CourseList.java
@@ -1,5 +1,6 @@
 package courses;
 
+import departments.DepartmentList;
 import utility.HttpReader;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
  * @author JohnN05
  */
 public class CourseList {
+    private static final int COURSE_ID_LENGTH = 7;
 
     private List<Course> courses;
 
@@ -19,5 +21,28 @@ public class CourseList {
 
     public List<Course> getCourses() {
         return courses;
+    }
+
+    /**
+     * Checks if the text fits the description of a Course ID
+     *
+     * @param text the text being checked
+     * @return true if first four characters are a Department while the last three characters are numbers.
+     * Otherwise, returns false.
+     */
+    public static boolean isCourseID(String text){
+
+        if(text.length() != COURSE_ID_LENGTH || DepartmentList.matchDepartment(text.substring(0, 4)) == null){
+            return false;
+        }else {
+
+            try {
+                Integer.parseInt(text.substring(4));
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
