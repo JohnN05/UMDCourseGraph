54c9181fe7e350c532a9b47550c70390e7b13b3d
John N <106115048+JohnN05@users.noreply.github.com>
2023-12-28 10:08:01 -0500

    Added offline accesses to DepartmentList

--- src/main/java/departments/DepartmentList.java
+++ src/main/java/departments/DepartmentList.java
@@ -1,8 +1,11 @@
 package departments;
 
 import utility.HttpReader;
+import utility.ObjectLoader;
 
 import java.util.List;
+
+import static utility.ObjectLoader.load;
 
 /**
  * Provides the client with a set of UMD's departments with umd.io API
@@ -12,9 +15,19 @@ import java.util.List;
 
 public class DepartmentList {
     private static final List<Department> allDepartments;
+    private static final String OFFLINE_LIST_PATH = "departments/department_list";
 
     static{
-        allDepartments = HttpReader.requestDepartmentSet();
+
+        List<Department> list = null;
+        try {
+            list = HttpReader.requestDepartmentSet();
+            ObjectLoader.save(list, OFFLINE_LIST_PATH);
+
+        }catch(RuntimeException e){
+           list = (List<Department>) load(OFFLINE_LIST_PATH);
+        }
+        allDepartments = list;
     }
 
     public static List<Department> getAllDepartments() {
