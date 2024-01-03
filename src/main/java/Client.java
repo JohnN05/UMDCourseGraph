c5a85aed8e29f43a96537bddbb654926963ca27f
John N <106115048+JohnN05@users.noreply.github.com>
2024-01-03 10:23:33 -0500

    Changed from RuntimeException to IOException.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,13 +1,15 @@
 import courses.CourseList;
 import web_ui.CourseWeb;
 
+import java.io.IOException;
+
 /**
  * Client for CourseWeb
  *
  * @author JohnN05
  */
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CourseList courseList = new CourseList();
         new CourseWeb("main",courseList);
     }
